361dcceee5d9d55bc42876fb5b51d5c397b02339
nikitakonan <nikitakonan1991@gmail.com>
2024-09-11 15:31:59 +0400

    added docs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const pug = require('pug');
 const fs = require('fs');
 
+const resume = require('./resume.json')
+
 function updatePublicDoc(html) {
-  fs.writeFileSync('./public/index.html', html);
+  fs.writeFileSync('./docs/index.html', html);
 }
 
 const locals = {
   title: 'Hello, World',
+  resume,
 };
 
 pug.renderFile('./src/layout.pug', locals, (err, html) => {
